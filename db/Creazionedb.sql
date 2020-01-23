607a8c7e3fe3eb18493af0cd75efb3a0626f9dd3
fabioromano1 <51378941+fabioromano1@users.noreply.github.com>
2020-01-23 19:52:38 +0100

    Update Creazionedb.sql

--- db/Creazionedb.sql
+++ db/Creazionedb.sql
@@ -10,30 +10,30 @@ create table ricetta(
      Engine=Innodb;
 	
 create table ingrediente (
-     nome varchar(50) NOT NULL PRIMARY KEY,
+     nomeIngrediente varchar(50) NOT NULL PRIMARY KEY,
      quantita double,
      tipo enum('malto','luppoli','zucchero','acqua','lievito'),
      bloccato boolean)
      Engine=Innodb;
 	
 create table ricettaIngrediente(
-     ricetta varchar(50) NOT NULL,
-	 ingrediente varchar(50) NOT NULL,
-     PRIMARY KEY (ricetta, ingrediente),
-     FOREIGN KEY (ricetta) REFERENCES ricetta(nomeBirra),
-     FOREIGN KEY (ingrediente) REFERENCES ingrediente(nome))
+     nomeBirra varchar(50) NOT NULL,
+	 nomeIngrediente varchar(50) NOT NULL,
+     PRIMARY KEY (nomeBirra, ingrediente),
+     FOREIGN KEY (nomeBirra) REFERENCES ricetta(nomeBirra),
+     FOREIGN KEY (nomeIngrediente) REFERENCES ingrediente(nomeIngrediente))
      Engine = Innodb;
 	
 create table attrezzatura(
-     nome varchar(50) NOT NULL PRIMARY KEY,
+     nomeAttrezzatura varchar(50) NOT NULL PRIMARY KEY,
      portata decimal(3,2),
      tipo enum('tubo','fermentatore','cisterna'))
      Engine=Innodb;
 
 create table ricettaAttrezzatura(
-     ricetta varchar(50) NOT NULL,
-     attrezzatura varchar(50) NOT NULL,
-     PRIMARY KEY (ricetta, attrezzatura),
-     FOREIGN KEY (ricetta) REFERENCES ricetta (nomeBirra),
-     FOREIGN KEY (attrezzatura) REFERENCES attrezzatura (nome))
+     nomeBirra varchar(50) NOT NULL,
+     nomeAttrezzatura varchar(50) NOT NULL,
+     PRIMARY KEY (nomeBirra, nomeAttrezzatura),
+     FOREIGN KEY (nomeBirra) REFERENCES ricetta (nomeBirra),
+     FOREIGN KEY (nomeAttrezzatura) REFERENCES attrezzatura (nomeAttrezzatura))
      Engine=Innodb;
