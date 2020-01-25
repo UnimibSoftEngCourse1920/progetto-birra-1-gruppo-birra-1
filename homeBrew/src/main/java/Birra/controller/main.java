6a3469e8842ff901e9a35d9adbe3fc8ccac823cf
fabioromano1 <51378941+fabioromano1@users.noreply.github.com>
2020-01-25 15:20:29 +0100

    nulla

--- homeBrew/src/main/java/Birra/controller/main.java
+++ homeBrew/src/main/java/Birra/controller/main.java
@@ -1,24 +1,27 @@
 package Birra.controller;
+
 import java.util.HashMap;
 
 import Birra.model.*;
 
-public class main 
-{
-	public static void main(String[] args)
-	{
+public class main {
+
+	public static void main(String[] args) {
 		TipoAttrezzatura tipo = TipoAttrezzatura.TUBO;
 		TipoIngrediente tipoI = TipoIngrediente.MALTO;
-		Attrezzatura strumento=new Attrezzatura("tuobo1", 10.10, tipo);
-		Attrezzatura[] strumenti= {strumento};
-		Ingrediente i=new Ingrediente("malto", 3.20, false, tipoI);
+		Attrezzatura strumento = new Attrezzatura("tubo1", 10.10, tipo);
+		Attrezzatura[] strumenti = { strumento };
+
+		Ingrediente i = new Ingrediente("malto", 3.20, false, tipoI);
 		HashMap<Ingrediente, Double> ingredienti = new HashMap<>();
 		ingredienti.put(i, 0.5);
-		Nota nota =new Nota("nota1", "blablabla...");
-		Ricetta r=new Ricetta("stout", 3.30, "fare...", strumenti, ingredienti, null);
-		ControllerRicetta cr=new ControllerRicetta(new ControllerIngrediente(), new ControllerAttrezzatura());
-		//System.out.println(cr.getRicetta(r.getNomeBirra()));
-		//System.out.println(cr.getNota(r.getNomeBirra()));
+
+		Nota nota = new Nota("nota1", "blablabla...");
+		Ricetta r = new Ricetta("stout", 3.30, "fare...", strumenti, ingredienti, null);
+		ControllerRicetta cr = new ControllerRicetta(new ControllerIngrediente(), new ControllerAttrezzatura());
+		// System.out.println(cr.getRicetta(r.getNomeBirra()));
+		// System.out.println(cr.getNota(r.getNomeBirra()));
 		cr.eliminaRicetta(r.getNomeBirra());
 	}
+
 }
