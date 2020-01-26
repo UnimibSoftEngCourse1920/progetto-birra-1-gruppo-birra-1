103292f7661c85d90be0839a48ebd30cb35e5d88
fabioromano1 <51378941+fabioromano1@users.noreply.github.com>
2020-01-26 02:09:22 +0100

    Update FacadeController.java

--- homeBrew/src/main/java/Birra/controller/FacadeController.java
+++ homeBrew/src/main/java/Birra/controller/FacadeController.java
@@ -2,6 +2,7 @@ package Birra.controller;
 
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 
 import Birra.model.Attrezzatura;
 import Birra.model.Ingrediente;
@@ -10,6 +11,7 @@ import Birra.model.Ricetta;
 import Birra.model.TipoIngrediente;
 
 public class FacadeController {
+
 	private ControllerRicetta cr;
 	private ControllerIngrediente ci;
 	private RicetteDisponibili rd;
@@ -28,7 +30,7 @@ public class FacadeController {
 	public boolean aggiungiIngrediente(String nome, String quantita, boolean bloccato, String tipo)
 			throws IllegalArgumentException {
 		Ingrediente ingr = creaIngrediente(nome, quantita, bloccato, tipo);
-		
+
 		if (ci.getIngrediente(nome) != null)
 			return false;
 
@@ -43,12 +45,13 @@ public class FacadeController {
 
 	public boolean modificaIngrediente(String nome, String quantita, boolean bloccato, String tipo)
 			throws IllegalArgumentException {
-		Ingrediente ingr = creaIngrediente(nome, quantita, bloccato, tipo);
+		Ingrediente newIngr = creaIngrediente(nome, quantita, bloccato, tipo);
+		Ingrediente oldIngr = ci.getIngrediente(nome);
 		
-		if (ci.getIngrediente(nome) == null)
+		if (oldIngr == null || oldIngr.equals(newIngr))
 			return false;
 
-		ci.modificaIngrediente(ingr);
+		ci.modificaIngrediente(newIngr);
 		return true;
 	}
 
@@ -61,15 +64,13 @@ public class FacadeController {
 		if (titolo == null)
 			return null;
 		
-		if (descrizione == null)
-			throw new NullPointerException("Nessuna descrizione nella nota " + titolo);
-
+		Objects.requireNonNull(descrizione, "Nessuna descrizione nella nota " + titolo);
 		checkString(titolo, "Titolo della nota vuoto");
 		checkString(descrizione, "La nota " + titolo + " ha descrizione vuota");
 
 		return new Nota(titolo, descrizione);
 	}
-	
+
 	private Ricetta creaRicetta(String nomeBirra, String tempo, String procedimento, Attrezzatura[] strumenti,
 			Map<Ingrediente, Double> percentualiIngredienti, String titoloNota, String descrizioneNota)
 			throws IllegalArgumentException, NullPointerException {
@@ -79,24 +80,25 @@ public class FacadeController {
 
 		if (time <= 0)
 			throw new IllegalArgumentException("Tempo per la ricetta " + nomeBirra + " minore o uguale a 0");
-		
+
 		if (strumenti.length == 0)
 			throw new IllegalArgumentException("Nessuno strumento per la birra " + nomeBirra);
-		
+
 		if (percentualiIngredienti.isEmpty())
 			throw new IllegalArgumentException("Nessun ingrediente per la birra " + nomeBirra);
-		
+
 		double sommaPerc = 0;
-		
+
 		for (Entry<Ingrediente, Double> coppia : percentualiIngredienti.entrySet()) {
 			double perc = coppia.getValue();
-			
+
 			if (perc <= 0)
-				throw new IllegalArgumentException("Percentuale dell'ingrediente " + coppia.getKey().getNome() + " minore o uguale a 0");
-			
+				throw new IllegalArgumentException(
+						"Percentuale dell'ingrediente " + coppia.getKey().getNome() + " minore o uguale a 0");
+
 			sommaPerc += perc;
 		}
-		
+
 		if (Math.abs(sommaPerc - 1) > 0.001)
 			throw new IllegalArgumentException("Somma delle percentuali della ricetta " + nomeBirra + "diversa da 100");
 
@@ -124,21 +126,23 @@ public class FacadeController {
 	public boolean modificaRicetta(String nomeBirra, String tempo, String procedimento, Attrezzatura[] strumenti,
 			Map<Ingrediente, Double> percentualiIngredienti, String titoloNota, String descrizioneNota)
 			throws IllegalArgumentException {
-		Ricetta ricetta = creaRicetta(nomeBirra, tempo, procedimento, strumenti, percentualiIngredienti, titoloNota,
+		Ricetta newRicetta = creaRicetta(nomeBirra, tempo, procedimento, strumenti, percentualiIngredienti, titoloNota,
 				descrizioneNota);
+		Ricetta oldRicetta = cr.getRicetta(nomeBirra);
 
-		if (cr.getRicetta(nomeBirra) == null)
+		if (oldRicetta == null || oldRicetta.equals(newRicetta))
 			return false;
 
-		cr.modificaRicetta(ricetta);
-
+		cr.modificaRicetta(newRicetta);
 		return true;
 	}
 
-	public boolean aggiungiNota(String nomeBirra, String titolo, String descrizione) throws IllegalArgumentException, NullPointerException {
+	public boolean aggiungiNota(String nomeBirra, String titolo, String descrizione)
+			throws IllegalArgumentException, NullPointerException {
 		Nota nota = creaNota(titolo, descrizione);
+		Ricetta ricetta = cr.getRicetta(nomeBirra);
 
-		if (cr.getRicetta(nomeBirra) == null)
+		if (ricetta == null || Objects.equals(nota, ricetta.getNota()))
 			return false;
 
 		cr.aggiungiNota(nomeBirra, nota);
